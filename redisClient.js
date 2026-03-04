cadf95ffc95bec192b4976406183f24b6726171a
Ramit Koirala <ramitkoirala@Ramits-MacBook-Air.local>
2026-03-04 13:02:53 +0530

    fix redis connection using REDIS_URL

--- redisClient.js
+++ redisClient.js
@@ -1,7 +1,7 @@
 const { createClient } = require("redis");
 
 const client = createClient({
-  url: process.env.REDIS_URL || "redis://127.0.0.1:6379"
+  url: process.env.REDIS_URL
 });
 
 client.on("error", (err) => console.log("Redis Error:", err));
